60754bd36bca765e99c56e57c41da0d62ad32632
marmeebeau <marchrodriguez20@gmail.com>
2024-10-01 16:14:44 +0800

    Revert "event package"
    This reverts commit 2cae1406f96aed100a4afc3840274cb6017b691f.

--- backend/src/api/event-package/controllers/event-package.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-package controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::event-package.event-package');
--- backend/src/api/event-package/services/event-package.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-package service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::event-package.event-package');
