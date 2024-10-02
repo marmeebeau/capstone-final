f11c0b0ca4d0709176d3de1b8425afb728de0dba
marmeebeau <marchrodriguez20@gmail.com>
2024-10-02 18:13:00 +0800

    Delete backend directory

--- backend/src/api/coordinator/services/coordinator.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * coordinator service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::coordinator.coordinator');
