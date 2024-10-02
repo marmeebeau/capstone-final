20613228165b4796fe573d73ef77e336fdaa8a3a
marmeebeau <marchrodriguez20@gmail.com>
2024-10-02 18:13:20 +0800

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App /> // Only render the App since AuthProvider is already handled within App.js
-);
